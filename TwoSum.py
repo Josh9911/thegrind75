87c88b5df45a9ba9dfb4dc88104f2cedb82980c5
Josh9911 <89121307+Josh9911@users.noreply.github.com>
2023-02-19 15:39:06 +0800

    added in comments to explain the code

--- TwoSum.py
+++ TwoSum.py
@@ -1,8 +1,10 @@
-
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Iterate through the indexes of the list
         for i in range (len(nums)):
+            # iterate through each index of the list again
             for j in range(len(nums)):
+                # if result is the sum of two iterations, return the indexes if the indexes of the indexes are not the same
                 res = nums[i] + nums[j]
                 if res == target and i != j :
                     return [i,j]
