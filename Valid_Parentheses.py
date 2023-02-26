69620601e40640489082fd061c90a5a934b448d5
Josh9911 <89121307+Josh9911@users.noreply.github.com>
2023-02-26 16:15:57 +0800

    new solution

---
+++ Valid_Parentheses.py
@@ -0,0 +1,29 @@
+def isValid(s: str) -> bool:
+    stack = []
+
+    element_dict = {
+            """(""" : """)""",
+            """{""" : """}""",
+            """[""" : """]"""
+            }
+    
+    for element in s:
+        if element in element_dict.keys():
+            stack.append(element)
+        elif element not in element_dict.keys() and element_dict[stack.pop()] == element:
+            stack.pop()
+        else:
+            return False
+
+    return len(stack) == 0
+
+
+print(isValid(s="()"))
+
+print(isValid("()[]{}"))
+
+print(isValid("(]"))
+
+print(isValid("([)]"))
+
+           
